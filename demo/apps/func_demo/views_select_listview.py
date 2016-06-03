009ac71e21f7f25e5d67fda0a8c8cb6f11768c3c
limodou <limodou@gmail.com>
2016-06-03 16:22:04 +0800

    change example

--- demo/apps/func_demo/views_select_listview.py
+++ demo/apps/func_demo/views_select_listview.py
@@ -3,10 +3,33 @@ from uliweb import expose, functions
 
 @expose('/func')
 class FuncSelectListView(object):
+    def create_query(self, url):
+        from uliweb.form import UnicodeField, SelectField, StringField
+        from uliweb.utils.generic import QueryView
+
+        QueryForm = functions.get_form('QueryForm')
+
+        subject = StringField('标题')
+        fields = [('subject', subject)]
+        layout = [('subject',)]
+        query = QueryView('user', ok_url=url, fields=fields, layout=layout, form_cls=QueryForm)
+        return query
+
     def select_listview(self):
         from uliweb.utils.generic import SelectListView
         from sqlalchemy.sql import and_, or_, not_, select, func, true
         from uliweb.orm import reflect_table
+
+        Blog = reflect_table('blog')
+
+        #create query
+        query_view = self.create_query(url_for(self.select_listview))
+        c = query_view.run()
+
+        #处理条件
+        condition = None
+        if c.get('subject'):
+            condition = Blog.c.subject.like(c['subject']+'%')
 
         fields = [
             {'name':'subject', 'width':80, 'verbose_name':'标题'},
@@ -18,9 +41,7 @@ class FuncSelectListView(object):
 
         fields_convert_map = {'subject':_subject}
 
-        Blog = reflect_table('blog')
-
-        query = select([Blog.c.subject, Blog.c.author])
+        query = select([Blog.c.subject, Blog.c.author], condition)
 
         view =  SelectListView(condition=None, #order_by=order_by,
             fields_convert_map=fields_convert_map, fields=fields,
@@ -33,6 +54,6 @@ class FuncSelectListView(object):
 
         else:
             result = view.run()
-            result.update({#'query_form':query_view.form,
+            result.update({'query_form':query_view.form,
                            'table':view})
             return result
