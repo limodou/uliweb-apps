948cb884ce30b6ab0ec3c12f8b1a530c7fe09325
limodou <limodou@gmail.com>
2017-04-18 10:12:03 +0800

    Add file field

--- demo/apps/func_demo/models.py
+++ demo/apps/func_demo/models.py
@@ -6,6 +6,7 @@ class Blog(Model):
     subject = Field(str, max_length=500, verbose_name=u'标题')
     content = Field(TEXT, verbose_name=u'内容')
     author = Reference('user', verbose_name=u'作者')
+    attachment = Field(FILE, verbose_name=u'文件')
     created_time = Field(DATETIME, verbose_name=u'创建时间', auto_now_add=True)
     modified_time = Field(DATETIME, verbose_name=u'修改时间', auto_now_add=True, auto_now=True)
 
@@ -16,7 +17,7 @@ class Blog(Model):
         return '/blog/{}'.format(self.id)
 
     class AddForm:
-        fields = ['subject', 'content']
+        fields = ['subject', 'content', 'attachment']
 
     class Table:
         fields = [
