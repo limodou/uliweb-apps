257190af4ee74ce3779141d09df20b7bfda36324
limodou <limodou@gmail.com>
2017-05-23 22:16:28 +0800

    Add favicon.ico support

--- uliweb_apps/site/views.py
+++ uliweb_apps/site/views.py
@@ -4,3 +4,7 @@ from uliweb import expose, functions
 @expose('/', template='site/home.html')
 def home():
     return {}
+
+@expose('/favicon.ico')
+def favicon():
+    return redirect('/static/favicon.ico')
