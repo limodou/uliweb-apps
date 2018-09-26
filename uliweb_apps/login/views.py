c87deb4a4669d3b56e49a31f908c1714b2eb3e04
limodou <limodou@gmail.com>
2018-09-26 13:56:22 +0800

    Fix to py3

--- uliweb_apps/login/views.py
+++ uliweb_apps/login/views.py
@@ -1,12 +1,16 @@
 #coding=utf8
 
-from uliweb.core.SimpleFrame import functions, expose
+from uliweb.core.SimpleFrame import functions, expose, redirect
 from uliweb.i18n import ugettext_lazy as _
-import urllib
+from uliweb.utils._compat import import_
+
+unquote = import_('urllib.parse', 'unquote')
+
 
 def add_prefix(url):
     from uliweb import settings
     return settings.DOMAINS.static.get('url_prefix', '') + url or '/'
+
 
 def login():
     from uliweb.contrib.auth import login
@@ -20,28 +24,26 @@ def login():
 
     next = request.values.get('next')
     if not next:
-        next = request.referrer
-        if not next or (next and next.endswith('/login')):
-            next = add_prefix('/')
+        next = add_prefix('/')
     if request.method == 'GET':
         form.next.data = next
-        return {}
+        return {'next': next}
     if request.method == 'POST':
-        flag = form.validate(request.params)
+        flag = form.validate(request.values)
         if flag:
             f, d = functions.authenticate(username=form.username.data, password=form.password.data)
             if f:
                 request.session.remember = form.rememberme.data
                 login(form.username.data)
-                next = urllib.unquote(next)
+                next = unquote(next)
                 return redirect(next)
             else:
                 form.errors.update(d)
         if request.is_xhr:
-            return json({'success':False, 'errors':form.errors})
+            return json({'success': False, '_': 'Login Failed', 'errors': form.errors})
         else:
             msg = form.errors.get('_', '') or _('Login failed!')
-            return {'form':form, 'msg':str(msg)}
+            return {'form': form, 'msg': str(msg)}
 
 
 def register():
@@ -61,31 +63,32 @@ def register():
 
     if request.method == 'GET':
         form.next.data = next
-        return {'form':form, 'msg':''}
+        return {'form': form, 'msg': ''}
     if request.method == 'POST':
-        flag = form.validate(request.params)
+        flag = form.validate(request.values)
         if flag:
             from uliweb import settings
             f, d = create_user(username=form.username.data,
-                password=form.password.data,
-                auth_type=settings.AUTH.AUTH_TYPE_DEFAULT)
+                               password=form.password.data,
+                               auth_type=settings.AUTH.AUTH_TYPE_DEFAULT)
             if f:
-                #add auto login support 2012/03/23
+                # add auto login support 2012/03/23
                 login(d)
-                next = urllib.unquote(next)
+                next = unquote(next)
                 return redirect(next)
             else:
                 form.errors.update(d)
 
         if request.is_xhr:
-            return json({'success':False, 'errors':form.errors})
+            return json({'success': False, '_': 'Register Failed', 'errors': form.errors})
         else:
             msg = form.errors.get('_', '') or _('Register failed!')
-            return {'form':form, 'msg':str(msg)}
+            return {'form': form, 'msg': str(msg)}
+
 
 def logout():
     from uliweb.contrib.auth import logout as out
     from uliweb import settings
     out()
-    next = urllib.unquote(request.POST.get('next', add_prefix('/')))
+    next = unquote(request.POST.get('next', add_prefix('/')))
     return redirect(next)
